cc6af62a219eb6166bc291eae42fc788e94f939a
ethan grossman <ethangrossman3@gmail.com>
2023-04-03 13:53:34 -0500

    finished exercise

--- app.js
+++ app.js
@@ -3,8 +3,28 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
 
-// Exercise 2
+const firstName = "Ethan";
+let lastName = "Grossman";
+var age = 30;
 
-// Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log(`${language} was first released in ${createdYear}`);
+
+let product = "T-shirt";
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
